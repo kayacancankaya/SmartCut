1fdaa23617c4aea016c134a049c546de4a26316c
User <User@NUCBOXM3>
2025-09-25 17:50:25 +0300

    cutting plan logic

--- SmartCut.Shared/Models/DTOs/CuttingPlanDTO.cs
+++ SmartCut.Shared/Models/DTOs/CuttingPlanDTO.cs
@@ -6,11 +6,15 @@ namespace SmartCut.Shared.Models.DTOs
     {
         public int Id { get; set; }
         public int Status { get; set; }
+        public Block? Block { get; set; } = new();
         public string Explanation { get; set; } = string.Empty;
         public List<CutEntryDTO> CutEntries { get; set; } = new();
+        public List<CutEntryDTO> NotCuttedEntries { get; set; } = new();
+        public float TotalVolume { get; set; }
         public float ScrapVolume { get; set; }
         [Required]
         public float PercentFulfilled { get; set; }
         public bool ShowDetails { get; set; } = false;
+        public bool ShowNotIncludedDetails { get; set; } = false;
     }
 }
